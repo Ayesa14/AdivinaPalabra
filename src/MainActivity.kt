f83381621defc1bf0f4d66c5c291c66acde51483
Aye14 <amarinconde@danielcastelao.org>
2025-01-29 14:05:52 +0100

    modificando la main

--- src/MainActivity.kt
+++ src/MainActivity.kt
@@ -1,14 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+package com.example.adivinapalabra
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.AdivinaPalabra.ui.theme.AdivinaPalabraTheme
+import com.example.adivinapalabra.MyViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewModel: MyViewModel = MyViewModel()
+
+        //enableEdgeToEdge()
+        setContent {
+            AdivinaPalabraTheme {
+                // A surface container using the 'background' color from the theme
+                UI(model = viewModel)
+            }
+        }
     }
 }
